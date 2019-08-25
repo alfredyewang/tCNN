ef16746bbac8f2e507115048add31e12fc71a4b4
yewang <yewang@auburn.edu>
2019-08-24 20:17:09 -0500

    update tCNN

--- examples.R
+++ examples.R
@@ -1,17 +1,16 @@
-library(Rcpp)
-library(dirmult)
+#library(Rcpp)
+#library(dirmult)
 library(ape)
-library(ade4)
-library(MASS)
-library(vegan)
+#library(ade4)
+#library(MASS)
+#library(vegan)
 source('R/tCNN.R')
 load('./twinsgut.Rdata')
 age=data.obj$meta.dat$age
 tree=data.obj$tree
-dim(data.obj$meta.dat)
-dim(data.obj$otu.tab)
+#dim(data.obj$meta.dat)
+#dim(data.obj$otu.tab)
 sex= data.obj$meta.dat$sex
-
 sex = as.integer(sex)
 sex = sex-2
 sex[sex==-1] <- NA
@@ -45,6 +44,4 @@ x_train = z[1:cut,]
 y_train = y[1:cut]
 x_test = z[(cut+1):dim(z)[1],]
 y_test = y[(cut+1):dim(z)[1]]
-
-
 tCNN(x_train=x_train,y_train=y_train,x_test=x_test,y_test=y_test,C=C,nCluster=20,num_classes=2,batch_size=16,epochs=20,num_filters=32,window_size=16,strides_size=8,dropout_rate=0.5,fc1_units=64,fc1_activate_function='relu',fc2_units=32,fc2_activate_function='relu',fc3_units=8,fc3_activate_function='relu')
